11671b5e59bde8b5985d09201dec66c374d89740
anshsangal12 <sangalansh4561@gmail.com>
2026-04-27 20:01:28 +0530

    Add TSP implementation using dynamic programming
    Implement dynamic programming solution for TSP.

---
+++ EST_Practical/travellingSalesMan.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n = 4;
+
+int dist[4][4] = {
+    {0, 10, 15, 20},
+    {10, 0, 35, 25},
+    {15, 35, 0, 30},
+    {20, 25, 30, 0}
+};
+
+int dp[1 << 4][4];
+
+int tsp(int mask, int pos) {
+    if (mask == (1 << n) - 1)
+        return dist[pos][0];
+
+    if (dp[mask][pos] != -1)
+        return dp[mask][pos];
+
+    int ans = INT_MAX;
+
+    for (int city = 0; city < n; city++) {
+        if ((mask & (1 << city)) == 0) {
+            int newAns = dist[pos][city] +
+                         tsp(mask | (1 << city), city);
+
+            ans = min(ans, newAns);
+        }
+    }
+
+    return dp[mask][pos] = ans;
+}
+
+int main() {
+    memset(dp, -1, sizeof(dp));
+
+    cout << "Minimum TSP cost: " << tsp(1, 0);
+
+    return 0;
+}
